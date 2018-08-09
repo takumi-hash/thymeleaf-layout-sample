3e957dd4d5537e07a8b49d3517b25735d3f1f464
takumi-hash <39505885+takumi-hash@users.noreply.github.com>
2018-08-09 16:54:28 +0900

    git init

---
+++ src/main/java/com/example/samplelayout/SamplelayoutApplication.java
@@ -0,0 +1,12 @@
+package com.example.samplelayout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamplelayoutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SamplelayoutApplication.class, args);
+    }
+}
